d10427ae3d55f09c53cda4e76ed3c7871bb07ed1
Matthias Lau <myclau@gmail.com>
2022-09-13 17:59:45 +0800

    draft

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # bash-log-analysis-example
+
---
+++ scenario01.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+source="./example-log.txt"
+
+
+#Get Most IP in two date
+
+date_start="01/Jun/2019"
+date_end="01/Jun/2019"
+#echo ${date_start//\//\\/}
+#echo ${date_end//\//\\/}
+
+# assume both day exist in the log, other wise need a while loop keep finding the exist date end
+# cat source | grep line date between | get ip field | unique and do count | sort field 1 first (count) field 2 (ip) | print first line | cut ip out
+
+# cut by tab cut -d$'\t'
+
+cat $source | sed -n '/'"${date_start//\//\\/}"'/,/'"${date_end//\//\\/}"'/p' | cut -d ' ' -f 1 | uniq -c | sort -k 1nr,1 -k 2r,2 | sort -c #| head -1 | cut -d ' ' -f 4
+
+
+
+#sort usage
+
+# Numeric sort (general)	-g / –general-numeric-sort
+    # general-numeric
+    # support for scientific notation
+    # 0.1234e4 = 1234
+# Numeric sort (human)	-h / –human-numeric-sort
+    # human-numeric
+    # 1.234K = 1234
+# Numeric	-n / –numeric-sort
+    # numeric
+    # … < -1 < 0 < 1 < …
+# Month	-M / –month-sort
+    # month
+    # Unknown < Jan < Feb < … < Nov < Dec
+# Random	-r / –random-sort
+    # random
+# Version	-V / –version-sort
+    # version
+
+# -t ' ' <-- new seperator
+
+# LC_ALL=C sort ascii.txt sort by ascii
+
+# F1.C1[sorting options],F2.C2
+# The F1 and F2 stand for the field indexes. In this case, they are 1 for the 1st field.
+# C1 is the character index within field F1 to begin the sort comparison. If we don’t define a C1, the comparison starts from the 1st character of the field F1.
+# C2 is the character index within field F2 to end the sort comparison. If we omit C2, the sorting comparison ends at the last character of the field F2.
+# In our example, to exclude the titles from the Name field, the sorting comparison should start from the 4th character. Therefore, we have “-k 1.4,1″.
+
+# https://www.baeldung.com/linux/sort-command
